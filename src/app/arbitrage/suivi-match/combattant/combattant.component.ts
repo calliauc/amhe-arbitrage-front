84ab53226954768200653911d90b595c74d726c4
calliauc <clement@calliau.io>
2025-01-12 14:30:22 +0100

    add dockerfile

--- src/app/arbitrage/suivi-match/combattant/combattant.component.ts
+++ src/app/arbitrage/suivi-match/combattant/combattant.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Combattant } from '../../../shared/models/combattant';
+import { ClubPipe } from '../../../shared/pipes/club.pipe';
 
 @Component({
   selector: 'app-combattant',
   standalone: true,
-  imports: [],
+  imports: [ClubPipe],
   templateUrl: './combattant.component.html',
   styleUrl: './combattant.component.css',
 })
